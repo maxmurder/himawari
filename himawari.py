fbd639c62d4667e858bc7f0886912970f2a9891e
Clayton Campbell <ccampbell@mstar.io>
2016-02-04 15:18:56 -0800

    added commandline arguments and multiple output formats

--- himawari.py
+++ himawari.py
@@ -5,12 +5,30 @@ from json import loads
 from time import strptime, strftime
 from PIL import Image
 from urllib.request import urlopen
-
-level = 20
+import argparse
 
 def main():
-	height = 550;
-	width = 550;
+
+	level = 4
+	height = 550
+	width = 550
+	outfile = ''
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-l", "--lod", choices=[4,8,16,20], help="Set level of detail for image. Valid values: 4, 8, 16, 20", type=int)
+	parser.add_argument("-o", "--out", help="Name of output file.")
+
+	args = parser.parse_args()
+	if args.lod == 8:
+		level = 8
+	elif args.lod == 16:
+		level = 16
+	elif args.lod ==20:
+		level = 20
+
+	if args.out is not None:
+		outfile = args.out
+
 
 	with urlopen("http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json") as latest_json:	
 		latest = strptime(loads(latest_json.read().decode("utf-8"))["date"], "%Y-%m-%d %H:%M:%S")
@@ -19,7 +37,7 @@ def main():
 
 	url_format = "http://himawari8.nict.go.jp/img/D531106/{}d/{}/{}_{}_{}.png"
 
-	png = Image.new('RGB',(width*level, height*level))
+	img = Image.new('RGB',(width*level, height*level))
 
 	print("Downloading: 0/{}".format(level*level), end="\r")
 
@@ -30,12 +48,28 @@ def main():
 
 			tile = Image.open(BytesIO(tileData))
 
-			png.paste(tile, (width*x, height*y, width*(x+1), height*(y+1)))
+			img.paste(tile, (width*x, height*y, width*(x+1), height*(y+1)))
 			print ("Downloading: {}/{}".format(x*level + y + 1, level*level), end="\r")
 
-	print("Finished")
+	print("\nFinished\n")
 
-	png.save(strftime("%Y-%m-%d-%H%M%S",latest) + ".png", "PNG")
+	if outfile == '':
+		outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".png"
+
+	if outfile[-4:] == ".png":
+		img.save(outfile, "PNG")
+	elif outfile[-4:] == ".jpg":
+		img.save(outfile, "JPEG")
+	elif outfile[-4:] == ".bmp":
+		img.save(outfile, "BMP")
+	elif outfile[-5:] == ".tiff":
+		img.save(outfile, "TIFF")
+	elif outfile[-4:] == ".pcx":
+		img.save(outfile, "PCX")
+	elif outfile[-4:] == ".ppm":
+		img.save(outfile, "PPM")
+	else:		
+		img.save(outfile + ".png", "PNG")
 
 if __name__ == "__main__":
 	main()
