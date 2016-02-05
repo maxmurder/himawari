9130924e0b7fac13ec0938e7566fc4585cc49522
maxmurder <rusty657@shaw.ca>
2016-02-05 00:08:18 -0800

    made setBackground.sh work in cron

--- setBackground.sh
+++ setBackground.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# export DBUS_SESSION_BUS_ADDRESS environment variable
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
 python3 himawari.py -o /tmp/himawariBG.png
 gsettings set org.gnome.desktop.background picture-uri file:///tmp/himawariBG.png
 gsettings set org.gnome.desktop.background picture-options scaled
-
-echo 'Done'
