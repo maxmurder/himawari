bbf0f71b42a1fb51481baf131a2ebc232d08dd9e
Clayton Campbell <ccampbell@mstar.io>
2016-02-04 16:23:51 -0800

    added the rest of the formats, added target format argument

--- himawari.py
+++ himawari.py
@@ -13,22 +13,20 @@ def main():
 	height = 550
 	width = 550
 	outfile = ''
+	format = ''
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-l", "--lod", choices=[4,8,16,20], help="Set level of detail for image. Valid values: 4, 8, 16, 20", type=int)
+	parser.add_argument("-f", "--format", choices=["png","jpg","bmp","tiff","pcx","ppm","im","eps","gif","spi","webp"], help="Output format.")
 	parser.add_argument("-o", "--out", help="Name of output file.")
 
 	args = parser.parse_args()
-	if args.lod == 8:
-		level = 8
-	elif args.lod == 16:
-		level = 16
-	elif args.lod ==20:
-		level = 20
-
+	if args.lod is not None:
+		level = args.lod
 	if args.out is not None:
 		outfile = args.out
-
+	if args.format is not None:
+		format = args.format
 
 	with urlopen("http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json") as latest_json:	
 		latest = strptime(loads(latest_json.read().decode("utf-8"))["date"], "%Y-%m-%d %H:%M:%S")
@@ -54,7 +52,30 @@ def main():
 	print("\nFinished\n")
 
 	if outfile == '':
-		outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".png"
+		if format == "png":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".png"
+		elif format == "jpg":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".jpg"
+		elif format == "bmp":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".bmp"
+		elif format == "tiff":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".tiff"
+		elif format == "pcx":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".pcx"
+		elif format == "ppm":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".ppm"
+		elif format == "im":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".im"
+		elif format == "eps":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".eps"
+		elif format == "gif":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".gif"
+		elif format == "spi":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".spi"
+		elif format == "webp":
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".webp"
+		else:
+			outfile = strftime("%Y-%m-%d-%H%M%S",latest) + ".png"
 
 	if outfile[-4:] == ".png":
 		img.save(outfile, "PNG")
@@ -68,6 +89,16 @@ def main():
 		img.save(outfile, "PCX")
 	elif outfile[-4:] == ".ppm":
 		img.save(outfile, "PPM")
+	elif outfile[-3:] == ".im":
+		img.save(outfile, "IM")
+	elif outfile[-4:] == ".eps":
+		img.save(outfile, "EPS")
+	elif outfile[-4:] == ".gif":
+		img.save(outfile, "GIF")
+	elif outfile[-4:] == ".spi":
+		img.save(outfile, "SPIDER")
+	elif outfile[-5:] == ".webp":
+		img.save(outfile, "WEBP")
 	else:		
 		img.save(outfile + ".png", "PNG")
 
