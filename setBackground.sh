fd09016602a543a1cfbf4776ee164aebaa5d09a1
maxmurder <rusty657@shaw.ca>
2016-02-04 22:46:52 -0800

    Removed context from setBackground.sh

--- setBackground.sh
+++ setBackground.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 ~/Development/himawari/himawari.py -o /tmp/himawariBG.png
+python3 himawari.py -o /tmp/himawariBG.png
 gsettings set org.gnome.desktop.background picture-uri file:///tmp/himawariBG.png
 gsettings set org.gnome.desktop.background picture-options scaled
 
