cb3d5680acfef947ddb2a836ef292047d6bd7ca0
Clayton Campbell <ccampbell@mstar.io>
2016-04-27 12:55:37 -0700

    Renamed updateBackgroundMac.sh

--- mac/updateBackgroundMac.sh
+++ mac/updateBackgroundMac.sh
@@ -3,3 +3,5 @@
 python3 /opt/scripts/himawari/himawari.py -o /tmp/himawariBG.png
 osascript /opt/scripts/himawari/setDesktops.applescript
 killall Dock
+
+cp /tmp/himawariBG.png /Users/ccampbell/Pictures/himawari/himawariBG.png
