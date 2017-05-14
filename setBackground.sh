80e4f586f77ab72517af0438025cd755d1719cb9
rusty <rusty@rpi>
2017-05-14 14:58:19 -0700

    Set environment so the scripth works in cron

--- setBackground.sh
+++ setBackground.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export DISPLAY=':0'
+export XAUTHORITY='$HOME/.Xauthority'
+
 /usr/bin/python3 /usr/local/bin/himawari.py -o /tmp/himawariBG.png
 /usr/bin/feh --bg-max /tmp/himawariBG.png
 /bin/chmod 666 /tmp/himawariBG.png
