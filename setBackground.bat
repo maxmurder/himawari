64ad1c28b085988d71edb79cee7a3d453a31560e
urist <urist6005@gmail.com>
2021-02-06 17:55:19 -0800

    add mixed output to setbackground script

--- setBackground.bat
+++ setBackground.bat
@@ -3,9 +3,12 @@ REM download and copy latest himawari image to a background folder for use in sl
 
 C:\python39\python.exe C:\Users\Urist\Development\himawari\himawari.py -o C:\Users\Urist\Pictures\bg\himawari\himawari_01.png
 copy C:\Users\Urist\Pictures\bg\himawari\himawari_01.png C:\Users\Urist\Pictures\bg\himawari\himawari_02.png
+copy C:\Users\Urist\Pictures\bg\himawari\himawari_01.png C:\Users\Urist\Pictures\bg\mixed\himawari.png
 
 C:\python39\python.exe C:\Users\Urist\Development\himawari\rammb.py -s goes-16 -z 1 -d C:\Users\Urist\Pictures\bg\goes-16\ -f goes-16_01.png
 copy C:\Users\Urist\Pictures\bg\goes-16\goes-16_01.png C:\Users\Urist\Pictures\bg\goes-16\goes-16_02.png
+copy C:\Users\Urist\Pictures\bg\goes-16\goes-16_01.png C:\Users\Urist\Pictures\bg\mixed\goes-16.png
 
 C:\python39\python.exe C:\Users\Urist\Development\himawari\rammb.py -s goes-17 -z 1 -d C:\Users\Urist\Pictures\bg\goes-17\ -f goes-17_01.png
 copy C:\Users\Urist\Pictures\bg\goes-17\goes-17_01.png C:\Users\Urist\Pictures\bg\goes-17\goes-17_02.png
+copy C:\Users\Urist\Pictures\bg\goes-17\goes-17_01.png C:\Users\Urist\Pictures\bg\mixed\goes-17.png
