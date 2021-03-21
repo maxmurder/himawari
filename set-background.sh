74a0d03116cab60e019e4c6cd62fed3a16c145ce
maxmurder <urist6005@gmial.com>
2021-03-20 20:35:58 -0700

    add set-background script

---
+++ set-background.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PID=$(id --real --user)
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$PID/bus"
+
+/usr/bin/python3 /usr/local/bin/rammb.py -s goes-17 -z 1 -d /tmp/ -f goes-17.png
+/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:////tmp/goes-17.png
+
+exit 0
