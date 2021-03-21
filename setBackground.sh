494d53c4e1659f34e5b052fe95351f3efed48f62
rusty <urist6005@gmial.com>
2021-03-20 20:31:15 -0700

    update for ubuntu 20.02

--- setBackground.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/bin/python3 /usr/local/bin/himawari.py -o /tmp/himawariBG.png
-/usr/bin/gconftool-2 --type string --set /desktop/gnome/background/picture_filename /tmp/himawariBG.png
-exit 0
--- setBackgroundFeh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export DISPLAY=':0'
-export XAUTHORITY='$HOME/.Xauthority'
-
-/usr/bin/python3 /usr/local/bin/himawari.py -o /tmp/himawariBG.png
-/usr/bin/feh --bg-max /tmp/himawariBG.png
-/bin/chmod 666 /tmp/himawariBG.png
-exit 0
